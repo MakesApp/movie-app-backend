64a338d5ee29c7f5fa8369ff6179631256327739
Pinchas Hodadad <40143951+pini85@users.noreply.github.com>
2023-01-22 12:06:01 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(5000, () => {
+  console.log(`Server started on port ${PORT}`);
+});
