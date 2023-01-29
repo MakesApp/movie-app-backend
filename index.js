3c9ba5ade0413eca4751268ffb0c6c45e2469994
ReaGol <reutgoldin@gmail.com>
2023-01-29 22:13:36 +0200

    mongoose connection

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 import express from "express";
-
+import './DB/mongoose.js'
+import cors from 'cors'
 const app = express();
+app.use(express.json());
+app.use(cors());
+const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(5000, () => {
+app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 });
+
