547599b3962e017b527634ac01cbb1bcdb157f00
m7madnat <m7madnatseh1999@gmail.com>
2023-01-30 13:32:56 +0200

    test23

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
-import express from "express";
-import './DB/mongoose.js'
-import cors from 'cors'
+import express from 'express';
+import './DB/mongoose.js';
+import cors from 'cors';
 const app = express();
 app.use(express.json());
 app.use(cors());
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get('/', (req, res) => {
+	res.send('Hello World!');
 });
 
 app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
-  console.log(`working together ..`);
+	console.log(`Server started on port ${PORT}`);
+	console.log(`working together ..`);
 });
-
