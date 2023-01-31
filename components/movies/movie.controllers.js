485a9553d0c354b88033ed18c035b8dc2549d724
arel hagag <areltv297@gmail.com>
2023-01-31 10:25:13 +0200

    begin validation routes

---
+++ components/movies/movie.controllers.js
@@ -0,0 +1,31 @@
+export const getMovies= async(req,res)=>{
+    // console.log(req.body);
+   return res.send("hello")
+}
+export const example= async(req,res)=>{
+    console.log(req.body);
+   return res.send("example")
+}
+
+
+
+export const getMovieById= async(req,res)=>{
+
+}
+
+
+export const addMovie= async(req,res)=>{
+
+}
+
+
+
+export const updateMovies= async(req,res)=>{
+
+}
+
+
+export const deleteMovies= async(req,res)=>{
+
+}
+
--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 import express from "express";
-import './DB/mongoose.js'
-import cors from 'cors'
+import "./services/DB/mongoose.js";
+import cors from "cors";
+import router from "./components/movies/movie.routes.js";
+
 const app = express();
 app.use(express.json());
 app.use(cors());
 const PORT = process.env.PORT || 5000;
-
+app.use("/api", router);
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
-  console.log(`working together ..`);
 });
-
