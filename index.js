7560cef7a6e3a871c378cd02cd0a3cde9db8cd2d
m7madnat <m7madnatseh1999@gmail.com>
2023-01-31 14:37:51 +0200

    cors

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
-import express from "express";
-import "./services/DB/mongoose.js";
-import cors from "cors";
-import router from "./components/movies/movie.routes.js";
+import express from 'express';
+import './services/DB/mongoose.js';
+import cors from 'cors';
+import router from './components/movies/movie.routes.js';
 
 const app = express();
 app.use(express.json());
+
+app.use((req, res, next) => {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Credentials', 'true');
+	res.header(
+		'Access-Control-Allow-Methods',
+		'GET, POST, PUT, DELETE, PATCH, HEAD'
+	);
+	res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+	next();
+});
+
+app.options('*', cors());
 app.use(cors());
 const PORT = process.env.PORT || 5000;
-app.use("/api", router);
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.use('/api', router);
+app.get('/', (req, res) => {
+	res.send('Hello World!');
 });
 
 app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
+	console.log(`Server started on port ${PORT}`);
 });
