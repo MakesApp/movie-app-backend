87efd0046f14deb02dbb1cd0f8edd5e5aa92f94c
m7madnat <m7madnatseh1999@gmail.com>
2023-02-06 15:31:31 +0200

    some changes for user schema

--- components/users/users.models.js
+++ components/users/users.models.js
@@ -1,15 +1,6 @@
 import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema({
-	email: {
-		type: String,
-		required: true,
-		unique: true,
-	},
-	password: {
-		type: String,
-		required: true,
-	},
 	favorites: [],
 });
 export const User = mongoose.model('User', userSchema);
