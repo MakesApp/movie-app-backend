dd05a1314877eda90eece5af123b450f5ee1b2d5
ReaGol <reutgoldin@gmail.com>
2023-02-06 10:50:05 +0200

    get latest movies is working

--- components/movies/movie.controllers.js
+++ components/movies/movie.controllers.js
@@ -1,80 +1,81 @@
-import logger from '../../services/logger/index.js';
-import Movie from './movie.models.js';
+// import logger from '../../services/logger/index.js';
+// import Movie from './movie.models.js';
 import axios from 'axios';
 import dotenv from 'dotenv';
 // import { customSort } from '../../utils/helper.js';
 
 dotenv.config();
 
-export const addMovie = async (req, res) => {
-	// console.log(req.body);
-	try {
-		const movie = await Movie.create(req.body);
-		logger.debug(`Movie added successfully: ${JSON.stringify(movie)}`);
-		res.status(200).send(movie);
-	} catch (err) {
-		logger.error(err.message);
+// export const addMovie = async (req, res) => {
+// 	// console.log(req.body);
+// 	try {
+// 		const movie = await Movie.create(req.body);
+// 		logger.debug(`Movie added successfully: ${JSON.stringify(movie)}`);
+// 		res.status(200).send(movie);
+// 	} catch (err) {
+// 		logger.error(err.message);
 
-		res.status(400).send({ error: err.message });
-	}
-};
+// 		res.status(400).send({ error: err.message });
+// 	}
+// };
 
-export const getAllMovies = async (req, res) => {
-	try {
-		const movies = await Movie.find({});
-		res.status(200).send({ movies });
-	} catch (error) {
-		res.status(500).send({ error: error.message });
-	}
-};
+// export const getAllMovies = async (req, res) => {
+// 	try {
+// 		const movies = await Movie.find({});
+// 		res.status(200).send({ movies });
+// 	} catch (error) {
+// 		res.status(500).send({ error: error.message });
+// 	}
+// };
 
-export const getMovieById = async (req, res) => {
-	try {
-		const movieId = req.params.id;
-		const movie = await Movie.findById(movieId);
-		if (!movie) {
-			throw Error('Movie not found');
-		}
-		res.status(200).send({ movie });
-	} catch (error) {
-		res.status(500).send({ error: error.message });
-	}
-};
+// export const getMovieById = async (req, res) => {
+// 	try {
+// 		const movieId = req.params.id;
+// 		const movie = await Movie.findById(movieId);
+// 		if (!movie) {
+// 			throw Error('Movie not found');
+// 		}
+// 		res.status(200).send({ movie });
+// 	} catch (error) {
+// 		res.status(500).send({ error: error.message });
+// 	}
+// };
 
-export const deleteMovie = async (req, res) => {
-	try {
-		const movieId = req.params.id;
-		const deletedMovie = await Movie.findByIdAndDelete(movieId);
-		if (!deletedMovie) {
-			throw Error('Movie not found');
-		}
-		res.status(200).send({ message: 'Movie deleted successfully' });
-	} catch (error) {
-		res.status(500).send({ error: error.message });
-	}
-};
+// export const deleteMovie = async (req, res) => {
+// 	try {
+// 		const movieId = req.params.id;
+// 		const deletedMovie = await Movie.findByIdAndDelete(movieId);
+// 		if (!deletedMovie) {
+// 			throw Error('Movie not found');
+// 		}
+// 		res.status(200).send({ message: 'Movie deleted successfully' });
+// 	} catch (error) {
+// 		res.status(500).send({ error: error.message });
+// 	}
+// };
 
-export const updateMovie = async (req, res) => {
-	try {
-		const movieId = req.params.id;
-		const updatedMovie = await Movie.findByIdAndUpdate(movieId, req.body, {
-			new: true,
-		});
-		if (!updatedMovie) {
-			res.status(404).send({ error: 'Movie not found' });
-		}
-		res.status(200).send({ movie: updatedMovie });
-	} catch (error) {
-		res.status(500).send({ error: error.message });
-	}
-};
+// export const updateMovie = async (req, res) => {
+// 	try {
+// 		const movieId = req.params.id;
+// 		const updatedMovie = await Movie.findByIdAndUpdate(movieId, req.body, {
+// 			new: true,
+// 		});
+// 		if (!updatedMovie) {
+// 			res.status(404).send({ error: 'Movie not found' });
+// 		}
+// 		res.status(200).send({ movie: updatedMovie });
+// 	} catch (error) {
+// 		res.status(500).send({ error: error.message });
+// 	}
+// };
 
 export const getLatestMovies = async (req, res) => {
 	const defaultPoster =
 		'https://images.unsplash.com/photo-1616530940355-351fabd9524b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8bW92aWUlMjBwb3N0ZXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60';
+	const { limit = 10 } = req.query;
 	try {
 		const tmdbResponse = await axios.get(
-			`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.TMDB_API_KEY}&language=en-US&page?limit=20`
+			`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.TMDB_API_KEY}&language=en-US&page?limit=${limit}`
 		);
 		const tmdbData = tmdbResponse.data;
 		const tmdbMovies = tmdbData.results.map((movie) => ({
@@ -97,7 +98,7 @@ export const getLatestMovies = async (req, res) => {
 			id: movie.imdbID,
 		}));
 
-		const latestMovies = [...omdbMovies, ...tmdbMovies].slice(0, 20);
+		const latestMovies = [...omdbMovies, ...tmdbMovies].slice(0, limit);
 		res.status(200).send(latestMovies);
 
 		// const { data } = await axios.get(
@@ -115,4 +116,23 @@ export const getLatestMovies = async (req, res) => {
 	}
 };
 
-// export const getDetailedMovie = async (req, res) => {};
+// export const getDetailedMovie = async (req, res) => {
+// 	try {
+// 		const { id } = req.body;
+
+// 		const tmdbResponse = await axios.get(
+// 			`https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.TMDB_API_KEY}`
+// 		);
+// 		const tmdbData = tmdbResponse.data;
+// 		console.log(tmdbData);
+
+// 		const omdbResponse = await axios.get(
+// 			`http://www.omdbapi.com/?apikey=${process.env.OMDB_API_KEY}&i=${omdbData.imdbID}`
+// 		);
+// 		const omdbData = omdbResponse.data;
+// 		console.log(omdbData);
+
+// 	} catch (error) {
+// 		res.status(404).send({ error: error.message });
+// 	}
+// };
--- components/movies/movie.routes.js
+++ components/movies/movie.routes.js
@@ -1,21 +1,23 @@
 import { Router } from 'express';
 
 import {
-	addMovie,
-	deleteMovie,
-	getMovieById,
-	getAllMovies,
-	updateMovie,
+	// getDetailedMovie,
+	// addMovie,
+	// deleteMovie,
+	// getMovieById,
+	// getAllMovies,
+	// updateMovie,
 	getLatestMovies,
 } from './movie.controllers.js';
 
 const router = Router();
 
 router.get('/movies/latest', getLatestMovies);
-router.get('/movies', getAllMovies);
-router.get('/movies/:id', getMovieById);
-router.post('/movies/add', addMovie);
-router.delete('/movies/:id', deleteMovie);
-router.patch('/movies/:id', updateMovie);
+// router.get('/movies/:id', getDetailedMovie)
+// router.get('/movies', getAllMovies);
+// router.get('/movies/:id', getMovieById);
+// router.post('/movies/add', addMovie);
+// router.delete('/movies/:id', deleteMovie);
+// router.patch('/movies/:id', updateMovie);
 
 export default router;
