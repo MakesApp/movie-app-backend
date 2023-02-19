2d1005b6e11b034eefa9a3031c27cc4287b21549
AmeerLalaDeveloper <laklaw100@gmail.com>
2023-02-19 11:55:32 +0200

    remove unused code

--- components/movies/movie.routes.js
+++ components/movies/movie.routes.js
@@ -1,20 +1,10 @@
 import { Router } from 'express';
-import { asyncWrapper } from '../../middleware/asyncWrapper.js';
 
 import { moviesController } from './movie.controllers.js';
-import { Movie } from './movie.model.js';
 
 const movieRouter = Router();
 
 movieRouter.get('/latest', moviesController.getLatestMovies);
 movieRouter.get('/:id', moviesController.getDetailedMovie);
-movieRouter.post(
-	'/',
-	// eslint-disable-next-line no-unused-vars
-	asyncWrapper(async (req, res, next) => {
-		const movie = await Movie.create(req.body);
-		console.log(movie);
-		res.status(200).send(movie);
-	})
-);
+
 export default movieRouter;
