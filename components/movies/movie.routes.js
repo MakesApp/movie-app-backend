00b2edd80b4f85da5b332862b52aac912e90f9ef
AmeerLalaDeveloper <laklaw100@gmail.com>
2023-02-21 16:51:40 +0200

    add new route

--- components/movies/movie.routes.js
+++ components/movies/movie.routes.js
@@ -5,6 +5,7 @@ import { moviesController } from './movie.controllers.js';
 const movieRouter = Router();
 
 movieRouter.get('/latest', moviesController.getLatestMovies);
+movieRouter.get('/getTopMovies', moviesController.getTopMovies);
 movieRouter.get('/:id', moviesController.getDetailedMovie);
 movieRouter.get('/search/movie', moviesController.searchMovie);
 
